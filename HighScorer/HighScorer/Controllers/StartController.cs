b8d02c26d629c86208757ac6cb3d558bd88e6306
Erik Nordenhök <nordenh@kth.se>
2017-02-21 23:10:14 +0100

    Some more files

---
+++ HighScorer/HighScorer/Controllers/StartController.cs
@@ -0,0 +1,21 @@
+﻿using HighScorer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HighScorer.Controllers
+{
+    public class StartController : ApiController
+    {
+        [HttpGet, Route("api/GetPlayableGames")]
+        public IHttpActionResult GetPlayableGames()
+        {
+            return Ok(new List<PlayableGame> { 
+                new PlayableGame {Name="Bowling", Link = "/bowling"}
+            });
+        }
+    }
+}
